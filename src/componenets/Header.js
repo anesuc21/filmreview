d1c05c79365c96d73d277f9b239c23d2392a4926
Walter Chibowora <walter.chibowora@students.mq.edu.au>
2026-05-16 16:56:31 +1000

    text colour

--- src/componenets/Header.js
+++ src/componenets/Header.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export const Header = () => {
     const[hidden,setHidden]=useState(true);
-    const activeClass = "text-base block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500";
+    const activeClass = "text-base block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-500 md:p-0 md:dark:text-blue-500";
     const inActiveClass = "text-base block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
     const navigate = useNavigate();
     const handleSubmit = (event) =>{
